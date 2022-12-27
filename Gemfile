73a3cfb8329170e4bde5f90c4373f31a49b4e785
kuwamura <kuwamura@miwanoMacBook-Pro-2.local>
2022-12-27 17:30:24 +0900

    aaaa

---
+++ Gemfile
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+source "https://rubygems.org"
+
+# Specify your gem's dependencies in str_convert_utils.gemspec
+gemspec
+
+gem "rake", "~> 13.0"
---
+++ lib/str_convert_utils.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require_relative "str_convert_utils/version"
+require "str_convert_utils/cli" 
+
+module StrConvertUtils
+  class Error < StandardError; end
+  # Your code goes here...
+end
